49a959c12b1bddb78b69d8176bfe973f8bc9702d
gorok <hetznecker2@gmail.com>
2021-11-29 15:54:11 +0100

    Init

---
+++ cfx_crawler_remake/Program.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cfx_crawler_remake
+{
+    class Program
+    {
+
+        private static string safeDir;
+        private static bool replace;
+
+        private static int fetchInterval;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            browserStart:
+            Console.WriteLine("Select the folder where you want to safe the files to");
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            browserDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            browserDialog.Description = "Select a folder";
+            DialogResult result = browserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                safeDir = browserDialog.SelectedPath;
+            }
+            else
+            {
+                Console.Clear();
+                goto browserStart;
+            }
+
+            Console.Clear();
+            Console.WriteLine("[n]Do you want to override existing files? (y/n)");
+            string yNo = Console.ReadLine();
+            Console.Clear();
+            if(yNo.ToLower() == "y")
+            {
+                replace = true;
+                Console.WriteLine("Replacing enabled");
+            }
+            else
+            {
+                replace = false;
+                Console.WriteLine("Replacing disabled");
+            }
+            Thread.Sleep(1000);
+
+            setInterval:
+            Console.Clear();
+            Console.WriteLine("Set fetch interval in minutes:");
+
+            string input = Console.ReadLine();
+            try
+            { 
+                fetchInterval = int.Parse(input);
+                fetchInterval = fetchInterval * 60;
+            }
+            catch (Exception)
+            {
+                goto setInterval;
+            }
+
+
+            Console.Clear();
+            Console.WriteLine("Fetching every " + (fetchInterval / 60) + " minutes");
+            Thread.Sleep(2500);
+            Console.Clear();
+            startCrawling();
+
+
+        }
+
+        private static Dictionary<string, string> nameUrl = new Dictionary<string, string>();
+        private static List<string> pages = new List<string>();
+        private static void startCrawling()
+        {
+            pages.Add("https://forum.cfx.re/c/development/releases/7");
+            pages.Add("https://forum.cfx.re/c/development/releases/7/l/latest?order=posts");
+            pages.Add("https://forum.cfx.re/c/development/releases/7/l/top");
+            pages.Add("https://forum.cfx.re/c/development/releases/7/l/latest?order=activity");
+            pages.Add("https://forum.cfx.re/c/development/releases/7/l/top?order=activity");
+            recrawl:
+            Console.Clear();
+            try
+            {
+
+                foreach(string page in pages)
+                {
+                    List<string> fetchUrls = getUrlList(readHtml(page));
+
+                    foreach(string url in fetchUrls.ToList())
+                    {
+                        Thread.Sleep(25);
+                        // Iterate through all founds urls to get only the valid ones
+                        string html = readHtml(url);
+                        Thread.Sleep(50);
+                        getDownloadLink(html, url);
+                        Thread.Sleep(50);
+                        Console.Clear();
+                        Console.WriteLine("Found " + nameUrl.ToList().Count);
+                        Thread.Sleep(50);
+                    }
+
+
+                    Console.WriteLine("Downloading resources of page..");
+                    Thread.Sleep(1250);
+                    Console.Clear();
+
+                    foreach(string str in nameUrl.Keys)
+                    {
+                        //str -> url
+                        //val -> fileName
+
+                        if (str.Contains("citizenfx"))
+                            continue;
+
+                        downloadFile(str, nameUrl[str]);
+                        Thread.Sleep(250);
+
+
+                    }
+
+                    nameUrl.Clear();
+                    System.GC.Collect();
+                }
+
+                Console.Clear();
+                System.GC.Collect();
+                int fetchCopy = fetchInterval;
+                while (fetchCopy > 1)
+                {
+                    Console.WriteLine("Fetching again in " + fetchCopy + " seconds");
+                    Thread.Sleep(10000);
+                    fetchCopy -= 10;
+                }
+                goto recrawl;
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        private static string parseGit(string str)
+        {
+            /*
+             * Downloadable link:
+             *  https://api.github.com/repos/username/reponame/zipball
+             * 
+             */
+
+            //normal received link:
+            // https://github.com/mat0ta/tota_scooter
+            // malformed link:
+            // https://github.com/Newtonzz/PassionRP-Public/tree/master/TightThreads
+
+            // https://github.com/FiveM-Scripts/Cops_FiveM/releases/latest
+
+            //    0   1  2              3             4       5        6
+
+            // https://github.com/citizenfx/project-lambdamenu/releases/download/3.13.2017/LAMBDAMENU_3.13.2017.zip
+
+            //https://github.com/gimicze/firescript/wiki/Commands-&amp;-Usage
+
+            try
+            {
+                string[] split = str.Split('/');
+                string username, reponame;
+                username = split[3];
+                reponame = split[4];
+                string baseUri = "https://api.github.com/repos/" + username + "/" + reponame + "/zipball";
+                return baseUri;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return "";
+            }
+
+        
+        }
+
+
+
+
+        private static string readHtml(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            WebRequest webRequest = (WebRequest)httpWebRequest;
+            webRequest.Proxy = null;
+            WebResponse webResponse = webRequest.GetResponse();
+            StreamReader sr = new StreamReader(webResponse.GetResponseStream());
+            string htmlStr = sr.ReadToEnd();
+            return htmlStr;
+        }
+
+        private static List<string> getUrlList(string html)
+        {
+            string linkedUrl;
+            List<string> urlList = new List<string>();
+
+            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+            foreach (var match in regexLink.Matches(html))
+            {
+                if (!urlList.Contains(match.ToString()))
+                {
+                    linkedUrl = GetLinkedUrl(match.ToString());
+                    if (linkedUrl.Contains("/t/"))
+                        urlList.Add(linkedUrl);
+                }
+            }
+            return urlList;
+        }
+
+        private static string GetLinkedUrl(string url)
+        {
+            if (!url.Contains("https://"))
+            {
+                url = "https://forum.cfx.re" + url;
+            }
+            return url;
+        }
+
+
+        public static void getDownloadLink(string htmlStr, string website)
+        {
+            try
+            {
+                string linkedUrl;
+                List<string> urlList = new List<string>();
+
+                Regex regexLink = new Regex("(?<=\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+                foreach (var match in regexLink.Matches(htmlStr))
+                {
+                    if (!urlList.Contains(match.ToString()))
+                    {
+                        linkedUrl = GetLinkedUrl(match.ToString());
+                        if (linkedUrl.Contains("/uploads/short-url/") | linkedUrl.Contains("github.com"))
+                        {
+                            urlList.Add(linkedUrl);
+
+
+                            /*
+                             * https://forum.cfx.re/t/release-cindys-flower-shop-mlo/1773193
+                             *    0   1     2       3     4                               5
+                             */
+                            string[] linkSplit = website.Split('/');
+
+                            if (!nameUrl.ContainsKey(linkedUrl) && !nameUrl.ContainsValue(linkSplit[4]))
+                            {
+                                if (linkedUrl.Contains("github"))
+                                {
+                                    nameUrl.Add(parseGit(linkedUrl), linkSplit[4]);
+                                }
+                                else
+                                {
+                                    nameUrl.Add(linkedUrl, linkSplit[4]);
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+        }
+
+        private static void downloadFile(string uri, string filename)
+        {
+            string extension;
+            if (uri.Contains("github"))
+                extension = ".zip";
+            else
+                extension = Path.GetExtension(uri);
+
+            string filePath = safeDir + @"\leak_studio_" + filename + extension;
+
+            if (!replace && File.Exists(filePath))
+            {
+                Console.WriteLine("[SKIP] File [" + filename + "] already exists");
+                return;
+            }
+
+
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    Console.WriteLine("Downloading [" + filename + "]");
+                    client.Headers.Add("user-agent", "simplec0de");
+                    Console.WriteLine(uri);
+                    client.DownloadFile(uri, filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERROR] Failed to download [" + filename + "]");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+    }
+}
---
+++ cfx_crawler_remake/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("cfx_crawler_remake")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cfx_crawler_remake")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("d492dd4d-a4a8-44ad-8df0-6b26aaa49f93")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
